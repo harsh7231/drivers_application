b5a97659c4b0171dac3aebec4f0351696e66c361
Harsh Kumar rattewal <harshrattewal@gmail.com>
2023-06-13 20:53:01 +0530

    Initial commit

---
+++ lib/mainScreens/main_screen.dart
@@ -0,0 +1,93 @@
+import 'package:drivers_application/tabPages/earning_tab.dart';
+import 'package:drivers_application/tabPages/home_tab.dart';
+import 'package:drivers_application/tabPages/profile_tab.dart';
+import 'package:drivers_application/tabPages/ratings_tab.dart';
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget
+{
+
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+
+
+
+
+
+class _MainScreenState extends State<MainScreen> with SingleTickerProviderStateMixin
+{
+  TabController? tabController;
+  int selectedIndex =0;
+
+
+  onItemClicked(int index)
+  {
+    setState(() {
+      selectedIndex=index;
+      tabController!.index=selectedIndex;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    tabController=TabController(length: 4 , vsync: this);
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TabBarView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: tabController,
+        children: const[
+          HomeTabPage(),
+          EarningsTabPage(),
+          RatingsTabPage(),
+          ProfileTabPage(),
+
+
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+
+
+          BottomNavigationBarItem
+            (icon: Icon(Icons.home),
+            label: "Home"
+          ),
+
+          BottomNavigationBarItem
+            (icon: Icon(Icons.credit_card),
+              label: "Earnings"
+          ),
+
+          BottomNavigationBarItem
+            (icon: Icon(Icons.star),
+              label: "Ratings"
+          ),
+
+          BottomNavigationBarItem
+            (icon: Icon(Icons.person),
+              label: "Account"
+          ),
+        ],
+        unselectedItemColor: Colors.white54,
+        selectedItemColor: Colors.white,
+        backgroundColor: Colors.black,
+        type:BottomNavigationBarType.fixed,
+        selectedLabelStyle: const TextStyle(fontSize: 14),
+        showUnselectedLabels: true,
+        currentIndex:selectedIndex ,
+        onTap: onItemClicked,
+
+      ),
+    );
+  }
+}
